688edb6ff67855b25b3f407e9e4e369f41a7f88a
Michele Mesiti <michele.mesiti@swansea.ac.uk>
2019-07-22 11:17:01 +0100

    Added script to stitch everything without makefile (quite simpler)

---
+++ Scripts/stitch_everything.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This might be actually simpler than using the makefile for this.
+for dir in ../Data/thirring_runs1/all_dirs/Ls* 
+do 
+    . ../Scripts/lib.sh&&cd .&&../ProtocolUtils/log stitch_together_wrapper $dir 1
+done
+for dir in ../Data/thirring_runs2/all_dirs/Ls* 
+do
+    . ../Scripts/lib.sh&&cd .&&../ProtocolUtils/log stitch_together_wrapper $dir 2
+done
+
+#find all_dirs/ | xargs cat 2>/dev/null| md5sum
