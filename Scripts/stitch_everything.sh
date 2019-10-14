ab791213dd597fb47c6e9100f1aae67efe953d46
Michele Mesiti <michele.mesiti@swansea.ac.uk>
2019-10-15 00:41:14 +0100

    stitch_everything script now takes care of L=16

--- Scripts/stitch_everything.sh
+++ Scripts/stitch_everything.sh
@@ -9,5 +9,10 @@ for dir in ../Data/thirring_runs2/all_dirs/Ls*
 do
     . ../Scripts/lib.sh&&cd .&&../ProtocolUtils/log stitch_together_wrapper $dir 2
 done
+for dir in ../Data/16/all_dirs/Ls* 
+do
+    . ../Scripts/lib.sh&&cd .&&../ProtocolUtils/log stitch_together_wrapper $dir 1
+done
+
 
 #find all_dirs/ | xargs cat 2>/dev/null| md5sum
