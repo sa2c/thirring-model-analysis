4f118e861515392058155b5f14e6180fa7b6758b
Michele Mesiti <michele.mesiti@swansea.ac.uk>
2019-11-04 14:40:58 +0000

    Corrected issue using 0.* instead of float for beta

--- Scripts/extrapolation_library.py
+++ Scripts/extrapolation_library.py
@@ -4,7 +4,6 @@ import numpy as np
 pbp_inf_dir = 'psibarpsi_extrapolated'
 pbp_inf_filename_pretty = 'psibarpsi_extrapolated_pretty'
 
-
 output_columns = [
     'last_pbp', 'last_pbp_e', 'constant', 'constant_e', 'alpha', 'alpha_e',
     'A', 'A_e', 'beta', 'L', 'mass', 'chisq', 'ndof'
@@ -12,10 +11,16 @@ output_columns = [
 
 
 def fit_output_filename_format(analysis_settings_filename, L, beta, mass):
-    return os.path.join(pbp_inf_dir,
-            f'{analysis_settings_filename}_{L}_{float(beta):1.6f}_{float(mass):1.6f}')
+    if type(beta) == float:
+        return os.path.join(
+            pbp_inf_dir,
+            f'{analysis_settings_filename}_{L}_{float(beta):1.6f}_{float(mass):1.6f}'
+        )
+    elif type(beta) == str:
+        return os.path.join(
+            pbp_inf_dir,
+            f'{analysis_settings_filename}_{L}_{beta}_{float(mass):1.6f}')
 
 
 def expexpression(A, alpha, constant, x):
     return A * np.exp(-alpha * x) + constant
-
