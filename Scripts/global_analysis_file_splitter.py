c414c6dbac9ea0a2784cd4c1fe37a3ba0340ec20
Michele Mesiti <michele.mesiti@swansea.ac.uk>
2019-11-17 15:11:26 +0000

    Now all new runs are discoverable. More python, less bash.
    - michele runs 2.0 still are done via bash (still working)
    - makefile is broken (it is too static anyway, it must be recreated
      every time the structure of the graph changes)
    - fort.200.analysis.set must be manually updated with new runs.
    - the directory layout and the structure of the files for micheles'
      data in Analsysis has changes. This is reflected in
      fort.200.analysis.set

--- Scripts/global_analysis_file_splitter.py
+++ Scripts/global_analysis_file_splitter.py
@@ -3,8 +3,9 @@ import pandas as pd
 from sys import argv
 import os
 import lib
-
-data = lib.filelist_parser(argv[1]).reset_index()
+from functools import reduce
+import numpy as np
+from collections import namedtuple
 
 
 def filenameoutput(df, analysis_settings_filename):
@@ -16,13 +17,43 @@ def filenameoutput(df, analysis_settings_filename):
     return f'L{L}Ls{Ls}.beta{beta:1.6f}.m{mass:1.6f}.{analysis_settings_filename}'
 
 
-def savedf(df):
-    filename = os.path.join(
-        'analysis_setting_split', filenameoutput(df, argv[1]))
+def savedf(df, directory):
+    filename = os.path.join(directory, filenameoutput(df, argv[1]))
     print(f"Writing {filename}")
     print(len(df))
-    df.to_csv(filename, sep='\t')
+    df.to_csv(filename, sep='\t', index=False)
 
 
-os.makedirs('analysis_setting_split',exist_ok=True)
-data.groupby(by=['L', 'Ls', 'beta', 'mass']).apply(savedf)
+def get_parameter_groups(analysis_settings_df):
+    return analysis_settings_df.loc[:, ['L', 'Ls', 'beta', 'mass'
+                                        ]].drop_duplicates().itertuples()
+
+
+def select_relevant_runs(analysis_settings_df: pd.DataFrame,
+                         params: namedtuple ):
+    conditions = [
+        analysis_settings_df[name] == getattr(params, name)
+        for name in params._fields if name in analysis_settings_df.columns
+    ]
+
+    return analysis_settings_df.loc[reduce(np.logical_and, conditions[1:],
+                                           conditions[0]), :]
+
+
+def separate_all_runs(analysis_settings_df):
+    return [
+        select_relevant_runs(analysis_settings_df, params)
+        for params in get_parameter_groups(analysis_settings_df)
+    ]
+
+
+if __name__ == '__main__':
+    directory = 'analysis_setting_split'
+    data = lib.filelist_parser(argv[1]).reset_index()
+    os.makedirs(directory, exist_ok=True)
+
+    for df in separate_all_runs(data):
+        savedf(df,directory)
+
+    #data.groupby(
+    #    by=['L', 'Ls', 'beta', 'mass']).apply(lambda df: savedf(df, directory))
---
+++ Scripts/global_michele_script1.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+import os
+import re
+import stitch_lib as sl
+import pandas as pd
+import logging
+
+michele_pattern = re.compile(
+    r'Ls(?P<Ls>[0-9]+).*beta(?P<beta>0.[0-9]+).*m(?P<m>0.[0-9]+)$'
+)  # this is just for the stem
+
+fort100cols = [
+    'real_psibarpsi1', 'aimag_psibarpsi1', 'real_psibarpsi2',
+    'aimag_psibarpsi2'
+]
+fort200cols = ["psibarpsi", "susclsing"]
+fort11cols = ['isweep', 'gaction', 'paction']
+
+michele_filename_info = [('conds',  r'fort.100', fort100cols),
+                       ('condensate', r'fort.200', fort200cols),
+                       ('bose', r'fort.11', fort11cols),
+                       ('out', r'output',None)]
+michele_filename_info = pd.DataFrame(
+    data=michele_filename_info, columns=['filetype',
+                                       'fortran_name', 'column_names']).set_index('filetype')
+
+michele_filename_info['regexp'] = michele_filename_info.fortran_name
+michele_filename_info['compiled_regexp'] = pd.Series(
+    [re.compile(v) for v in michele_filename_info.regexp],
+    index=michele_filename_info.index)
+
+def match_directory_name_michele(dirpath,copy):
+    match = re.search(michele_pattern, dirpath)
+    try:
+        resdict = {
+            'beta': float(match.group('beta')),
+            'Ls': int(match.group('Ls')),
+            'm': float(match.group('m')),
+            'copy': copy 
+        }
+        return resdict
+    except:
+        return None
+
+michele_additional_pattern = r'save[0-9]+/[0-9]{10}'
+michele_additional_pattern = re.compile(michele_additional_pattern)
+
+def filenames_found_per_type_fun_michele(dirpath):
+
+    res =  dict([(filetype, [
+        os.path.join(directory, fortran_name)
+        for (directory, _, filenames) in os.walk(dirpath)
+        if re.search(michele_additional_pattern,directory) is not None and fortran_name in filenames
+    ]) for (filetype,fortran_name) in zip(
+        michele_filename_info.index,
+        michele_filename_info.fortran_name)])
+
+    for k,v in res.items():
+       print(f"Found {len(v)} {k} files")
+
+
+    return res
+
+def simon_get_newdir_name(prefix, L, copy, Ls, beta, m):
+    return os.path_join('simon',
+            sl.get_newdir_name(prefix, L, copy, Ls, beta, m))
+
+def michele_paste_all(dirpath):
+    logging.info('michele_paste_all')
+    return sl.paste_all(dirpath,michele_filename_info,filenames_found_per_type_fun_michele)
+ 
+def process_michele_directory(directory,prefix,Lfun,copy):
+    sl.process_directory(directory,prefix,Lfun,
+            lambda x : match_directory_name_michele(x, copy),
+            sl.get_newdir_name,michele_paste_all,michele_filename_info)
+
+if __name__ == '__main__':
+
+    # TODO: Prefixes
+    for directory,_,_ in os.walk('../Data/thirring_runs1/all_dirs/'):
+        if match_directory_name_michele(directory,'1'):
+            process_michele_directory(directory,'michele',lambda x: 12,'1')
+
+    for directory,_,_ in os.walk('../Data/thirring_runs2/all_dirs/'):
+        if match_directory_name_michele(directory,'2'):
+            process_michele_directory(directory,'michele',lambda x: 12,'2')
+
+    for directory,_,_ in os.walk('../Data/16/all_dirs/'):
+        if match_directory_name_michele(directory,''):
+            process_michele_directory(directory,'michele',lambda x: 16,'')
+
+
+
