7e86773604175e939f12d6fe00f5bf57b9ad94c1
Michele Mesiti <michele.mesiti@gmail.com>
2020-07-03 23:11:34 +0100

    fixed bug with ordering

--- Scripts/fort_colnames.py
+++ Scripts/fort_colnames.py
@@ -6,10 +6,11 @@ fort200cols = ["psibarpsi", "susclsing"]
 fort11cols = ['isweep', 'gaction', 'paction']
 
 cols = {'fort.100':fort100cols, 'fort.200' : fort200cols, 'fort.11' : fort11cols}
-cols_new = {'fort.100':fort100cols_newcode, 'fort.200' : fort200cols, 'fort.11' : fort11cols}
 
 fort100cols_newcode = [
     'isweep_total',
     'real_psibarpsi1', 'aimag_psibarpsi1', 'real_psibarpsi2',
     'aimag_psibarpsi2'
 ]
+
+cols_new = {'fort.100':fort100cols_newcode, 'fort.200' : fort200cols, 'fort.11' : fort11cols}
